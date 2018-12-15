1f4ac182d38a1595f4b454ffcff873c572db6d12
lee1012116 <lee1012116@126.com>
2018-12-15 15:32:43 +0800

    Initial commit

---
+++ tensorflow_program_study/chapter_1/__init__.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+'''
+@author: caopeng
+@license: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+@contact: deamoncao100@gmail.com
+@software: garner
+@file: __init__.py.py
+@time: 18-12-13 下午9:17
+@desc:
+'''
